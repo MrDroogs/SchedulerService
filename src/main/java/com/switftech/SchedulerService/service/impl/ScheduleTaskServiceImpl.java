a9784b683b8965d626ba431348ee9c124965de10
sanket.acharya <sanket.acharya4@hotmail.com>
2024-04-04 11:14:30 +0545

    scheduler

--- src/main/java/com/switftech/SchedulerService/service/impl/ScheduleTaskServiceImpl.java
+++ src/main/java/com/switftech/SchedulerService/service/impl/ScheduleTaskServiceImpl.java
@@ -1,8 +1,6 @@
 package com.switftech.SchedulerService.service.impl;
 
 import com.switftech.SchedulerService.dto.Request.EventRequest;
-import com.switftech.SchedulerService.model.Event;
-import com.switftech.SchedulerService.model.Status;
 import com.switftech.SchedulerService.service.ScheduleTaskService;
 import com.switftech.SchedulerService.util.EventScheduler;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +8,6 @@ import org.quartz.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.ZonedDateTime;
@@ -30,27 +27,9 @@ public class ScheduleTaskServiceImpl implements ScheduleTaskService {
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
     }
-
-    @Scheduled(cron = "0 * * * * *")
-    public void scheduleTasks() {
+    public void scheduleEvent(EventRequest eventRequest) {
         try {
-            // Your existing logic here to fetch events
-
-            // Schedule event handling job
-            Event eventRequest = new Event();
-            eventRequest.setMessage("Sample Event");
-            eventRequest.setStatus(Status.SCHEDULED);
-            // Set other properties as needed
-
-            scheduleEvent(eventRequest);
-        } catch (Exception ex) {
-            logger.error("Failed to schedule tasks", ex);
-        }
-    }
-
-    public void scheduleEvent(Event eventRequest) {
-        try {
-            ZonedDateTime dateTime = ZonedDateTime.of(eventRequest.getDateTime(), eventRequest.getTimeZone().toZoneId());
+            ZonedDateTime dateTime = ZonedDateTime.of(eventRequest.getEventDateTime(), eventRequest.getTimeZone().toZoneId());
             if (dateTime.isBefore(ZonedDateTime.now())) {
                 logger.error("dateTime must be after current time");
                 return;
@@ -70,13 +49,14 @@ public class ScheduleTaskServiceImpl implements ScheduleTaskService {
         JobDataMap jobDataMap = new JobDataMap();
 
         jobDataMap.put("", eventRequest.getEventDateTime());
-        return JobBuilder.newJob((Class<? extends Job>) EventScheduler.class) // Assuming EventJob implements the Job interface
+        return JobBuilder.newJob()
                 .withIdentity(UUID.randomUUID().toString(), "event-jobs")
                 .withDescription("Handle Event Job")
                 .usingJobData(jobDataMap)
                 .storeDurably()
                 .build();
     }
+
 
 
     private Trigger buildJobTrigger(JobDetail jobDetail, ZonedDateTime startAt) {
